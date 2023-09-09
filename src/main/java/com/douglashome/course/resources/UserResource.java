7f375597fe1d2f4e17f4a38495753ca0221511cc
douglasfranca19 <francadouglas19@gmail.com>
2023-09-09 20:05:14 -0300

    User update

--- src/main/java/com/douglashome/course/resources/UserResource.java
+++ src/main/java/com/douglashome/course/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +32,7 @@ public class UserResource {
 		return ResponseEntity.ok().body(list);
 	}
 
-	@GetMapping(value = "/{Id}")
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 
 		User obj = service.findById(id);
@@ -46,10 +47,16 @@ public class UserResource {
 
 	}
 
-	@DeleteMapping(value = "/{Id}")
+	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
